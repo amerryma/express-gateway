5ff151f8e4f543dbd7c23230d1dfb89df2e0d752
Kevin Swiber <kswiber@gmail.com>
2017-09-07 15:52:38 -0700

    Moving plugin installation logic to lib/plugin-installer.js

--- bin/generators/plugins/install.js
+++ bin/generators/plugins/install.js
@@ -1,9 +1,5 @@
-const fs = require('fs');
-const os = require('os');
-const path = require('path');
-const { spawn } = require('child_process');
-const YAWN = require('yawn-yaml/cjs');
 const eg = require('../../eg');
+const PluginInstaller = require('../../../lib/plugin-installer');
 
 module.exports = class extends eg.Generator {
   constructor (args, opts) {
@@ -13,6 +9,7 @@ module.exports = class extends eg.Generator {
     this.pluginManifest = null;
     this.pluginOptions = null;
 
+    this.installer = PluginInstaller.create();
     this.enablePlugin = false;
     this.addPoliciesToWhitelist = false;
 
@@ -27,80 +24,18 @@ module.exports = class extends eg.Generator {
   }
 
   initializing () {
-    return new Promise(resolve => {
-      // manually spawn npm
-      // use --parseable flag to get tab-delimited output
-      // forward sterr to process.stderr
-      // capture stdout to get package name
-
-      let pluginPath = null;
-
-      const installArgs = [
-        'install', this.argv.package,
-        '--cache-min', 24 * 60 * 60,
-        '--parseable'
-      ];
-
-      const installOpts = {
-        cwd: this.env.cwd,
-        env: process.env,
-        stdio: ['ignore', 'pipe', 'inherit']
-      };
-
-      const npmInstall = spawn('npm', installArgs, installOpts);
-
-      npmInstall.on('error', _ => {
-        this.log.err('Cannot install', this.argv.package);
-      });
-
-      const bufs = [];
-      let len = 0;
-      npmInstall.stdout.on('readable', () => {
-        const buf = npmInstall.stdout.read();
-
-        if (buf) {
-          bufs.push(buf);
-          len += buf.length;
-        }
-      });
-
-      npmInstall.stdout.on('end', () => {
-        const lines = Buffer.concat(bufs, len)
-          .toString()
-          .trim()
-          .split('\n');
-
-        const output = lines[lines.length - 1].split('\t');
-
-        if (output.length < 4) {
-          this.log.error('Cannot parse npm output while installing plugin.');
-          process.exit();
-        }
-
-        this.pluginName = output[1];
-        pluginPath = path.join(this.env.cwd, output[3]);
-      });
-
-      npmInstall.on('exit', () => {
-        if (pluginPath) {
-          this.pluginManifest = require(pluginPath);
-          resolve();
-        }
-      });
+    return this.installer.runNPMInstallation({
+      packageSpecifier: this.argv.package,
+      cwd: this.env.cwd,
+      env: process.env
     });
   }
 
   prompting () {
-    const optionsMeta = this.pluginManifest.options || {};
+    const optionsMeta = this.installer.pluginManifest.options || {};
     const keys = Object.keys(optionsMeta);
 
-    const config = require('../../../lib/config');
-    const systemConfig = config.systemConfig;
-    const name = this.pluginManifest.name || this.pluginName;
-
-    const previousPluginOptions =
-      systemConfig.plugins && systemConfig.plugins[name] ?
-        systemConfig.plugins[name] : {};
+    const previousPluginOptions = this.installer.existingPluginOptions;
 
     const pluginQuestions = keys.map(key => {
       const schema = optionsMeta[key];
@@ -179,131 +114,11 @@ module.exports = class extends eg.Generator {
   }
 
   writing () {
-    // NOTE (kevinswiber): Updating YAML while maintaining presentation
-    // style is not easy.  We're using the YAWN library here, which has
-    // a decent approach given the current state of available YAML parsers,
-    // but it's far from perfect.  Take a look at existing YAWN issues
-    // before making any optimizations.  If any section of this code looks
-    // ugly or inefficient, it may be that way for a reason.
-    //
-    // ¯\_(ツ)_/¯
-    //
-    // https://github.com/mohsen1/yawn-yaml/issues
- 
-    const name = this.pluginManifest.name || this.pluginName;
-    const policyNames = this.pluginManifest.policies || [];
-
-    const config = require('../../../lib/config');
-
-    if (this.enablePlugin) {
-      const isJSON = config.systemConfigPath.toLowerCase().endsWith('.json');
-      const isYAML = !isJSON;
-
-      const systemConfig = fs.readFileSync(config.systemConfigPath);
-
-      // YAML-specific variables
-      let yawn = null;
-      let oldLength = null;
-
-      let obj = null;
-
-      if (isYAML) {
-        yawn = new YAWN(systemConfig.toString());
-        obj = Object.assign({}, yawn.json);
-
-        oldLength = obj.plugins ? null : yawn.yaml.length;
-      } else {
-        obj = JSON.parse(systemConfig.toString());
-      }
-
-      let plugins = obj.plugins || {};
-
-      if (!plugins.hasOwnProperty(name)) {
-        plugins[name] = null;
-      }
-
-      if (name !== this.pluginName) {
-        plugins[name] = plugins[name] || {};
-        plugins[name].package = this.pluginName;
-        obj.plugins = plugins;
-
-        if (isYAML) {
-          obj = this._updateYAML(obj, yawn);
-        }
-      }
-
-      if (this.pluginOptions) {
-        plugins[name] = plugins[name] || {};
-        const self = this;
-        const keys = Object.keys(self.pluginOptions);
-
-        // YAWN needs to be updated by smallest atomic unit
-        keys.forEach(key => {
-          plugins[name][key] = self.pluginOptions[key];
-          obj.plugins = plugins;
-
-          if (isYAML) {
-            obj = this._updateYAML(obj, yawn);
-            plugins = obj.plugins;
-          }
-        });
-      }
-
-      if (isYAML && oldLength) {
-        // add a line break before new plugins mapping
-        yawn.yaml = yawn.yaml.substr(0, oldLength) + os.EOL +
-          yawn.yaml.substr(oldLength);
-      }
-
-      const output = isYAML ? yawn.yaml.trim() : JSON.stringify(obj, null, 2);
-
-      fs.writeFileSync(config.systemConfigPath, output);
-    }
-
-    if (this.addPoliciesToWhitelist) {
-      const isJSON = config.gatewayConfigPath.toLowerCase().endsWith('.json');
-      const isYAML = !isJSON;
-
-      const gatewayConfig = fs.readFileSync(config.gatewayConfigPath);
-
-      // YAML-specific variable
-      let yawn = null;
-
-      let obj = null;
-
-      if (isYAML) {
-        yawn = new YAWN(gatewayConfig.toString());
-        obj = Object.assign({}, yawn.json);
-      } else {
-        obj = JSON.parse(gatewayConfig.toString());
-      }
-
-      let policies = obj.policies || [];
-
-      // YAWN reverses arrays
-      const correctedPolicyNames = isYAML ? policyNames.reverse() : policyNames;
-      
-      correctedPolicyNames.forEach(policy => {
-        if (policies.indexOf(policy) === -1) {
-          policies.push(policy);
-        }
-      });
-
-      obj.policies = policies;
-
-      if (isYAML) {
-        yawn.json = obj;
-      }
-
-      const output = isYAML ? yawn.yaml.trim() : JSON.stringify(obj, null, 2);
-
-      fs.writeFileSync(config.gatewayConfigPath, output);
-    }
-  }
-
-  _updateYAML(obj, yawn) {
-    yawn.json = obj;
-    return yawn.json;
+    return this.installer.updateConfigurationFiles({
+      pluginOptions: this.pluginOptions,
+      enablePlugin: this.enablePlugin,
+      addPoliciesToWhitelist: this.addPoliciesToWhitelist
+    });
   }
 
   end () {
